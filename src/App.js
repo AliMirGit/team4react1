5585a97f78ed413c7691c1817ba0c19c9dbf0e29
Troy Pullis <troy.pullis@dal.frb.org>
2023-11-20 14:06:02 -0600

    test2 - adding api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World test1';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>App.js calling API: {data}</div>;
 }
+
+
+//import React from 'react';
+
+//function App() {
+//  const value = 'World test1';
+//  return <div>Hello {value}</div>;
+//}
 
 export default App;
